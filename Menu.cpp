ceaa9fde5f2806080eeae141d9c011fb5db28c7f
Nicusor <crucerunicusor04@gmail.com>
2024-04-22 05:16:44 +0300

    Final

---
+++ Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nicusor43 on 4/22/24.
+//
+
+#include "Menu.h"
---
+++ Menu.h
@@ -0,0 +1,14 @@
+//
+// Created by nicusor43 on 4/22/24.
+//
+
+#ifndef OOPPROJ2_MENU_H
+#define OOPPROJ2_MENU_H
+
+
+class Menu {
+
+};
+
+
+#endif //OOPPROJ2_MENU_H
